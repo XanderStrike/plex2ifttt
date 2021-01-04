10ae1c166b88e2d3795922497190a251eda44c98
Alex Standke <astandke@vmware.com>
2021-01-04 05:37:24 +0000

    idk anymore

--- handler/handler.go
+++ handler/handler.go
@@ -38,7 +38,8 @@ func (h Handler) HandleEvent(user, player, event string) {
 		if err != nil {
 			panic(err)
 		}
-		now := h.Time().In(loc)
+		eight, _ := time.ParseDuration("-8h")
+		now := h.Time().In(loc).Add(eight) // this is some bullshit but it doesn't work otherwise???
 
 		p := sunrisesunset.Parameters{
 			Latitude:  -33.960,
@@ -52,13 +53,14 @@ func (h Handler) HandleEvent(user, player, event string) {
 			panic(err)
 		}
 
-		log.Println("Sunrise: ", sunrise.In(loc).Format("Mon 3:04PM"), "Sunset: ", sunset.In(loc).Format("Mon 3:04PM"), "Now: ", now.In(loc).Format("Mon 3:04PM"))
-		if sunrise.In(loc).Hour() > now.In(loc).Hour() && now.In(loc).Hour() < sunset.In(loc).Hour() {
-			log.Println("Send play (night)")
-			h.Requester("plex_play")
-		} else {
+		// what the actual fuck is going on with timezones
+		log.Println("Sunrise: ", sunrise.In(loc).Format("Mon 3:04PM"), "Sunset: ", sunset.In(loc).Format("Mon 3:04PM"), "Now: ", now.Format("Mon 3:04PM"))
+		if sunrise.In(loc).Hour() < now.In(loc).Hour() && now.In(loc).Hour() < sunset.In(loc).Hour() {
 			log.Println("Send play (day)")
 			h.Requester("plex_play_day")
+		} else {
+			log.Println("Send play (night)")
+			h.Requester("plex_play")
 		}
 	} else {
 		log.Println("Send pause")
--- main.go
+++ main.go
@@ -5,8 +5,10 @@ import (
 	"log"
 	"net/http"
 	"regexp"
+	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/kelvins/sunrisesunset"
 	"github.com/xanderstrike/plexhooks"
 	"github.com/xanderstrike/plexlights/handler"
 )
@@ -31,6 +33,27 @@ func hook(w http.ResponseWriter, r *http.Request) {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
+	loc, err := time.LoadLocation("America/Los_Angeles")
+	if err != nil {
+		panic(err)
+	}
+	now := time.Now()
+
+	p := sunrisesunset.Parameters{
+		Latitude:  -33.960,
+		Longitude: -118.351,
+		UtcOffset: -8.0,
+		Date:      now,
+	}
+
+	sunrise, sunset, err := p.GetSunriseSunset()
+	if err != nil {
+		panic(err)
+	}
+
+	// what the actual fuck is going on with timezones
+	eight, _ := time.ParseDuration("-8h")
+	log.Println("Sunrise: ", sunrise.In(loc).Format("Mon 3:04PM"), "Sunset: ", sunset.In(loc).Format("Mon 3:04PM"), "Now: ", now.Add(eight).Format("Mon 3:04PM"))
 	_, _ = w.Write([]byte("OK"))
 }
 
@@ -40,5 +63,6 @@ func main() {
 	router.HandleFunc("/", index)
 
 	log.Println("Now serving on 0.0.0.0:8080/hook")
+	log.Println("Version 0.0.2")
 	log.Fatal(http.ListenAndServe("0.0.0.0:8080", router))
 }
